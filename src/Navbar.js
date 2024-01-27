5d8cfd3a0be9d8d1e77b687c7f211f553f598c63
Ruth346 <78221918+Ruth346@users.noreply.github.com>
2024-01-27 17:46:55 +0000

    test

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return <nav className="nav">
     <a href="/" className="site-title">
-        artists support networks
+        artists support network
     </a>
     <ul>
         <li className="home">
